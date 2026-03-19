1c10d3c12f500af16d534d683f4b790b49a4d2f2
Josué <josue.rico@uabc.edu.mx>
2026-03-18 18:38:36 -0700

    Modificación en el constructor

--- src/main/java/Clases/SensorTemperatura.java
+++ src/main/java/Clases/SensorTemperatura.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class SensorTemperatura extends Sensor{
     double valorActual;
 
-    public SensorTemperatura(String modelo, String fabricante, double consumoEnergia, boolean encendido, String unidadMedida, boolean estaCalibrado, double valorActual) {
+    public SensorTemperatura(String modelo, String fabricante, double consumoEnergia, boolean encendido, String unidadMedida, boolean estaCalibrado) {
         super(modelo, fabricante, consumoEnergia, encendido, unidadMedida, estaCalibrado);
-        this.valorActual = valorActual;
+        this.valorActual = 0.0;
     }
-
+    @Override
     public double leerValor() {
         Random rmd = new Random();
         System.out.println("Leyendo temperatura ...");
